64dfd674ad28c1d6732a5fdef4995f98089ae953
Max Sum <maxsumprc@gmai.com>
2015-09-04 22:36:45 +0800

    Track for CUHK

--- bg.js
+++ bg.js
@@ -4,7 +4,18 @@ chrome.runtime.onMessage.addListener(function(msg, sender) {
         /* Enable the page-action for the requesting tab */
         chrome.pageAction.show(sender.tab.id);
         chrome.pageAction.onClicked.addListener(function(){
-          window.open(msg.link);
+            window.open(msg.link);
+            jQuery(
+                $.ajax({
+                    url:  'http://qianjian.tk/wp-admin/admin-ajax.php',
+                    type: 'post',
+                    data: { action: "zilla-likes", likes_id: "zilla-likes-1763", postfix: ""}
+                })
+            );
         });
     }
 });
+
+function track() {
+
+};
--- cuhk.js
+++ cuhk.js
@@ -10,22 +10,6 @@
 var test;
 var previouscomponent;
 
-function trackButton() {
-  //track();
-  console.debug('Clicked and tracked.');
-};
-
-function track() {
-  jQuery(
-    $.ajax({
-      url:  'http://qianjian.tk/wp-admin/admin-ajax.php',
-      type: 'post',
-      data: { action: "zilla-likes", likes_id: "zilla-likes-1763", postfix: ""}
-
-    })
-  );
-};
-
 function listener() {
     console.debug("listener fired.");
     jQuery(function($) {
@@ -210,12 +194,6 @@ function listener() {
           subject: "showPageAction",
           link:    'data:text/calendar;charset=utf8,' + encodeURIComponent(wrapICalContent(iCalContentArray.join('')))
         });
-        /*$('.PATRANSACTIONTITLE').append(
-          ' (<a href="#" id="downloadlink" onclick="window.open(\'data:text/calendar;charset=utf8,' +
-          encodeURIComponent(wrapICalContent(iCalContentArray.join(''))) +
-          '\');">Download Schedule</a>)'
-        );*/
-        //document.getElementById("downloadlink").addEventListener("click", trackButton);
         }
         else {
           console.debug("Length not > 0");
